b89609e77b85bfc374d42aa1be6ec25492ff6a7b
Brandon Butterworth <bbutterworth@FTI-NET.COM>
2019-11-21 22:56:48 -0500

    commit 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>Aliquam</h3>
+                                <h3>Test Update</h3>
                                 <p>Ipsum dolor sit amet</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
